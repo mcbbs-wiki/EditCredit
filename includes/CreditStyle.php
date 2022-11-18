41e14f3e9fddc88b181c52f1d03bb2bcc112df2b
litwak913 <litwak913@qq.com>
2022-11-18 12:51:32 +0800

    fix:mobile style

--- includes/CreditStyle.php
+++ includes/CreditStyle.php
@@ -5,17 +5,20 @@ namespace MediaWiki\Extension\EditCredit;
 use ResourceLoaderContext;
 use ResourceLoaderWikiModule;
 
-class CreditStyle extends ResourceLoaderWikiModule {
+class CreditStyle extends ResourceLoaderWikiModule
+{
     protected function getPages(ResourceLoaderContext $context)
     {
         $pages = [];
-        $pages['MediaWiki:EditCredit.css'] = [ 'type' => 'style' ];
+        $pages['MediaWiki:EditCredit.css'] = ['type' => 'style'];
         return $pages;
     }
-	public function getType() {
-		return self::LOAD_STYLES;
-	}
-	public function getGroup() {
-		return 'ext.editcredit';
-	}
+    public function getType()
+    {
+        return self::LOAD_STYLES;
+    }
+    public function getGroup()
+    {
+        return 'ext.editcredit';
+    }
 }
