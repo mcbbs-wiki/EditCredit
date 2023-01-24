a074628642b632b767c214364aecc59472f43b37
litwak913 <litwak913@163.com>
2023-01-24 17:54:12 +0800

    services

--- includes/EditCreditQuery.php
+++ includes/EditCreditQuery.php
@@ -5,25 +5,46 @@ namespace MediaWiki\Extension\EditCredit;
 use MediaWiki\Extension\EditCount\EditCountQuery;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\User\UserIdentity;
+use MediaWiki\User\ActorNormalization;
+use ConfigFactory;
+use Config;
+use MediaWiki\HookContainer\HookContainer;
+use Wikimedia\Rdbms\ILoadBalancer;
 
 class EditCreditQuery {
-	private $editCountQuery;
-	private $config;
+	private EditCountQuery $editCountQuery;
+	private ILoadBalancer $dbLoadBalancer;
+	private Config $config;
+	private HookContainer $hooks;
 
-	public function __construct( EditCountQuery $editCountQuery ) {
-		$this->editCountQuery = $editCountQuery;
-		$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'EditCredit' );
+	public function __construct( ActorNormalization $actorNormalization,
+	ILoadBalancer $dbLoadBalancer,ConfigFactory $configFactory,HookContainer $hookContainer ) {
+		$this->dbLoadBalancer = $dbLoadBalancer;
+		$this->editCountQuery = new EditCountQuery($actorNormalization,$dbLoadBalancer);
+		$this->config = $configFactory->makeConfig( 'EditCredit' );
+		$this->hooks = $hookContainer;
 	}
 
-	public function queryCredit( UserIdentity $user ) {
+	public function queryCredit(UserIdentity $user)
+	{
+		return $this->queryFromUserEdit($user);
+	}
+	private function queryFromCache(UserIdentity $user)
+	{
+		
+	}
+	private function queryFromTable(UserIdentity $user)
+	{
+		
+	}
+	private function queryFromUserEdit( UserIdentity $user ) {
 		$credit = 0;
 		$ns = $this->editCountQuery->queryAllNamespaces( $user );
-		$hooks = MediaWikiServices::getInstance()->getHookContainer();
-		@$hooks->run( "CalcUserCredit", [ $ns, &$credit ] );
+		@$this->hooks->run( "CalcUserCredit", [ $ns, &$credit ] );
 		return $credit;
 	}
 
-	public function queryLevelCSSClass( int $credit ) {
+	public function calcLevelCSSClass( int $credit ) {
 		$levelCSS = $this->config->get( 'CreditCSSClass' );
 		$class = '';
 		foreach ( $levelCSS as $least => $level ) {
@@ -35,7 +56,7 @@ class EditCreditQuery {
 		return $class;
 	}
 
-	public function queryLevel( int $credit ) {
+	public function calcLevel( int $credit ) {
 		$scores = $this->config->get( 'CreditLevels' );
 		$lvl = 0;
 		foreach ( $scores as $index => $score ) {
--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,29 +2,45 @@
 
 namespace MediaWiki\Extension\EditCredit;
 
+use ConfigFactory;
 use Html;
 use MediaWiki\Extension\EditCount\EditCountQuery;
 use MediaWiki\Hook\ParserFirstCallInitHook;
 use MediaWiki\User\ActorNormalization;
 use MediaWiki\User\UserIdentityLookup;
+use DatabaseUpdater;
 use Parser;
+use Exception;
+use MediaWiki\HookContainer\HookContainer;
 use PPFrame;
 use WikiMedia\Rdbms\ILoadBalancer;
 
 class Hooks implements ParserFirstCallInitHook {
 	private $userIdentityLookup;
 	private $editCreditQuery;
-
+	public static function onLoadExtensionSchemaUpdates(DatabaseUpdater $updater) {
+		$dir = __DIR__ . '/../sql';
+		$dbType = $updater->getDB()->getType();
+		if (!in_array($dbType, array('mysql'))) {
+				throw new Exception('Database type not currently supported');
+		}
+		$updater->addExtensionTable('edit_credit', "{$dir}/tables-generated.sql");
+		return true;
+	}
 	public function __construct(
 		ActorNormalization $actorNormalization,
 		ILoadBalancer $dbLoadBalancer,
-		UserIdentityLookup $userIdentityLookup
+		UserIdentityLookup $userIdentityLookup,
+		ConfigFactory $configFactory,
+		HookContainer $hookContainer
 	) {
 		$this->userIdentityLookup = $userIdentityLookup;
-		$this->editCreditQuery = new EditCreditQuery( new EditCountQuery(
+		$this->editCreditQuery = new EditCreditQuery( 
 			$actorNormalization,
-			$dbLoadBalancer
-		) );
+			$dbLoadBalancer,
+			$configFactory,
+			$hookContainer
+		 );
 	}
 
 	public function onParserFirstCallInit( $parser ) {
@@ -39,8 +55,8 @@ class Hooks implements ParserFirstCallInitHook {
 				return '';
 			}
 			$credit = $this->editCreditQuery->queryCredit( $user );
-			$level = $this->editCreditQuery->queryLevel( $credit );
-			$cssClass = $this->editCreditQuery->queryLevelCSSClass( $credit );
+			$level = $this->editCreditQuery->calcLevel( $credit );
+			$cssClass = $this->editCreditQuery->calcLevelCSSClass( $credit );
 			$type = $args['type'] ?? 'level';
 			if ( $type === 'level' ) {
 				$class = 'user-score-level ' . $cssClass;
