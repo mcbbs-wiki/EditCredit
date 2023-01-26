cef6134829d9295d13e794b171885ac24a4cb98f
litwak913 <litwak913@163.com>
2023-01-26 10:20:22 +0800

    style

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -6,7 +6,7 @@ use MediaWiki\MediaWikiServices;
 return [
 	'EditCredit.Query' => static function ( MediaWikiServices $services ): EditCreditQuery {
 		return new EditCreditQuery(
-			$services->getService('EditCountNeue.EditCountQuery'),
+			$services->getService( 'EditCountNeue.EditCountQuery' ),
 			$services->getDBLoadBalancer(),
 			$services->getConfigFactory(),
 			$services->getHookContainer()
